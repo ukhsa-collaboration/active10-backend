dd80ff09ad1a8d25cb171430ee79eb5b214563df
ademir <ademir@msit.co.uk>
2024-08-12 23:02:35 +0500

    User Walking Plan API CRUD

--- api/v1/walking_plans.py
+++ api/v1/walking_plans.py
@@ -1,28 +1,69 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from crud.walking_plan_crud import UserWalkingPlanCRUD
+from models import User
+from models.walking_plan import UserWalkingPlan
+from schemas.walking_plan import WalkingPlanRequestSchema, UserWalkingPlanResponseSchema
+from service.auth_service import get_authenticated_user_data
 
 router = APIRouter(prefix="/walking_plans", tags=["walking plans"])
 
 
-@router.post("/")
-async def create_walking_plan():
-    pass
+@router.post("/", response_model=UserWalkingPlanResponseSchema, status_code=201)
+async def create_walking_plan(
+        user: Annotated[User, Depends(get_authenticated_user_data)],
+        payload: WalkingPlanRequestSchema,
+        walking_plan_crud: Annotated[UserWalkingPlanCRUD, Depends()],
+):
+    existing_plan = walking_plan_crud.get_walking_plan_by_user_id(user.id)
+
+    if existing_plan:
+        raise HTTPException(status_code=400, detail="User walking plan already exists")
+
+    new_walking_plan = UserWalkingPlan(
+        user_id=user.id,
+        walking_plan_data=payload.walking_plan_data
+    )
+    created_walking_plan = walking_plan_crud.create_walking_plan(new_walking_plan)
+    return created_walking_plan
 
 
-@router.get("/{user_id}")
-async def get_walking_plans_by_user():
-    pass
+@router.get("/", response_model=UserWalkingPlanResponseSchema, status_code=200)
+async def get_user_walking_plan(
+        user: Annotated[User, Depends(get_authenticated_user_data)],
+        walking_plan_crud: Annotated[UserWalkingPlanCRUD, Depends()],
+):
+    walking_plan = walking_plan_crud.get_walking_plan_by_user_id(user.id)
+    if not walking_plan:
+        raise HTTPException(status_code=404, detail="User walking plan not found")
+    return walking_plan
 
 
-@router.get("/walking_plans/{user_id}/{plan_id}")
-async def get_walking_plan_by_id():
-    pass
+@router.put("/", response_model=UserWalkingPlanResponseSchema, status_code=200)
+async def update_walking_plan(
+        user: Annotated[User, Depends(get_authenticated_user_data)],
+        payload: WalkingPlanRequestSchema,
+        walking_plan_crud: Annotated[UserWalkingPlanCRUD, Depends()],
+):
+    user_walking_plan = walking_plan_crud.get_walking_plan_by_user_id(user.id)
+
+    if not user_walking_plan:
+        raise HTTPException(status_code=404, detail="User walking plan not found")
+
+    updated_walking_plan = walking_plan_crud.update_walking_plan(user_walking_plan, payload)
+    return updated_walking_plan
 
 
-@router.put("/{plan_id}")
-async def update_walking_plan(plan_id):
-    pass
+@router.delete("/", status_code=204)
+async def delete_walking_plan(
+        user: Annotated[User, Depends(get_authenticated_user_data)],
+        walking_plan_crud: Annotated[UserWalkingPlanCRUD, Depends()],
+):
+    user_walking_plan = walking_plan_crud.get_walking_plan_by_user_id(user.id)
 
+    if not user_walking_plan:
+        raise HTTPException(status_code=404, detail="User walking plan not found")
 
-@router.delete("/{user_id}/{plan_id}")
-async def delete_walking_plan(plan_id):
-    pass
+    walking_plan_crud.delete_walking_plan(user_walking_plan)
---
+++ crud/walking_plan_crud.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from db.session import get_db_session
+from models.walking_plan import UserWalkingPlan
+from schemas.walking_plan import WalkingPlanRequestSchema
+
+
+class UserWalkingPlanCRUD:
+    def __init__(self, db: Session = Depends(get_db_session)) -> None:
+        self.db = db
+
+    def create_walking_plan(self, walking_plan) -> UserWalkingPlan:
+        self.db.add(walking_plan)
+        self.db.commit()
+        self.db.refresh(walking_plan)
+
+        return walking_plan
+
+    def get_walking_plan_by_user_id(self, uuid: str) -> Union[UserWalkingPlan, None]:
+        return self.db.query(UserWalkingPlan).filter(UserWalkingPlan.user_id == uuid).first()
+
+    def update_walking_plan(self, walking_plan: UserWalkingPlan, payload: WalkingPlanRequestSchema) -> UserWalkingPlan:
+        walking_plan.walking_plan_data = payload.walking_plan_data
+        self.db.commit()
+        self.db.refresh(walking_plan)
+
+        return walking_plan
+
+    def delete_walking_plan(self, walking_plan: UserWalkingPlan) -> None:
+        self.db.delete(walking_plan)
+        self.db.commit()
