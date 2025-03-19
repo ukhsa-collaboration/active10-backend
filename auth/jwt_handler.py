a8b44850fe066359f979be868e4d047f6fe3a13f
ademir <ademir@msit.co.uk>
2025-03-19 14:14:02 +0500

    add jwt token expiry duration as env variable

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -7,11 +7,11 @@ from utils.base_config import config
 
 JWT_SECRET = config.auth_jwt_secret
 JWT_ALGORITHM = "HS256"
-TOKEN_EXPIRY_30_DAY_AS_SEC = 2592000
+JWT_TOKEN_EXPIRY_IN_SECONDS = config.auth_jwt_expiry_in_seconds
 
 
 def sign_jwt(user_id: str) -> Dict[str, str]:
-    payload = {"user_id": user_id, "exp": time.time() + TOKEN_EXPIRY_30_DAY_AS_SEC}
+    payload = {"user_id": user_id, "exp": time.time() + JWT_TOKEN_EXPIRY_IN_SECONDS}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
     return token
