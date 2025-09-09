460fba73f158a56fee618d2c72476782fb0c33dc
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2025-09-09 17:22:20 +0100

    formatting

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # DO NOT REMOVE BELOW LINES: They are necessary for model files to be included in auto-generation of migrations
-from .activity import * # noqa
-from .user import * # noqa
-from .walking_plan import * # noqa
-from .daily_target import * # noqa
-from .email_notification import * # noqa
+from .activity import *  # noqa
+from .user import *  # noqa
+from .walking_plan import *  # noqa
+from .daily_target import *  # noqa
+from .email_notification import *  # noqa
